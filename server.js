32b3655fb67ddc303efa378dc68d9acf7ba8be69
limononoil <63930000+limononoil@users.noreply.github.com>
2021-03-08 22:56:25 +0100

    add new routes and implement CR(UD) mongodb methods

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server started on Port: ${PORT}`);
+});
+
 // setup MongoDB
 const { MongoClient } = require("mongodb");
 
@@ -38,9 +43,47 @@ app.get("/mongo", (req, res) => {
     res.send("Welcome to Mongo!");
 });
 
+//* Query Documents
+app.get("/restaurants", (req, res) => {
+    const db = client.db(DB_NAME);
+    // select all documents in a collection
+    db.collection("restaurants").find({}).toArray((error, info) => {
+        if (error) throw error;
+        console.log(info);
+        res.send(info);
+    });
+});
 
-const PORT = process.env.PORT;
+app.get("/restaurants/:name", (req, res) => {
+    console.log(req.params);
+    const name = req.params.name;
+    const db = client.db(DB_NAME);
+    // The db.collection.findOne() method also performs a read operation to return a single document. Internally, the db.collection.findOne() method is the db.collection.find() method with a limit of 1.
+    db.collection("restaurants").findOne({ name }, (error, info) => {
+        if (error) throw error;
+        console.log(info);
+        res.send(info);
+    });
+});
 
-app.listen(PORT, () => {
-    console.log(`Server started on Port: ${PORT}`);
+app.post("/restaurants", (req, res) => {
+    const name = req.body.name;
+    const db = client.db(DB_NAME);
+    db.collection("restaurants").findOne({ name: req.body.name }, (error, info) => {
+        if (error) throw error;
+        res.send(info);
+    });
+});
+
+//* Create Operations
+app.post("/add", (req, res) => {
+    console.log("Add new restaurant");
+    const restaurantData = req.body;
+    const db = client.db(DB_NAME);
+    // insert a single document to a collection with the insertOne() method
+    db.collection("restaurants").insertOne(restaurantData, (error, info) => {
+        if (error) throw error;
+        console.log(info.ops[0]);
+        res.send(info.ops[0]);
+    });
 });
